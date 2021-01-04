e50b135043ee180a7ec7dedcab66af2146eb2538
Liam Murray <murrayl9797@gmail.com>
2021-01-04 12:16:29 -0500

    Slowish solution for Two City Scheduling

---
+++ problems/twoCityScheduling.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} costs
+ * @return {number}
+ */
+var twoCitySchedCost = function(costs) {
+  // Decompose into one sorted array
+  const complexArray = [];
+  let i = 0;
+
+  for (let [costA, costB] of costs) {
+      complexArray.push([costA, costB, costB - costA]);
+  }
+
+  complexArray.sort((a,b) => b[2] - a[2]);
+
+  // Once decomposed, add all A, then add B
+  const n = (costs.length / 2);
+  let countA = 0;
+  let sum = 0;
+
+  for (let [costA, costB, profitForA] of complexArray) {
+    if (countA < n) {
+      sum += costA;
+      countA++;
+    } else {
+      sum += costB;
+    }
+  }
+
+  // Return result
+  return sum;
+};
+
+const input = [[259,770],[448,54],[926,667],[184,139],[840,118],[577,469]]
+console.log(`Answer:`, twoCitySchedCost(input));
