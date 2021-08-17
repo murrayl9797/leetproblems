cfd6de21386356841625c6d5b90b1bf6ce555c23
Liam Murray <murrayl9797@gmail.com>
2021-08-17 13:01:32 -0400

    Partially understand iterative solution

--- problems/python/paintFence.py
+++ problems/python/paintFence.py
@@ -1,36 +1,23 @@
-class Solution:
-    def numWays(self, n: int, k: int) -> int:
-        self.total_count = 0
+class Solution(object):
+    def numWays(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        if k == 0 or n == 0:
+            return 0
+        if n == 1:
+            return k
 
-        # Set up recursive helper
-        def recurseFn(postsLeft, colors, prevQ):
+        same = k * 1
+        diff = k * (k - 1)
+        for _ in range(3, n + 1):
+            same, diff = diff, (same + diff) * (k - 1)
 
-            # Base case
-            if postsLeft == 0:
-                self.total_count += 1
-                return
+        return same + diff
 
-            # Recursive Cases
-            for color in range(colors):
-                copyQ = prevQ.copy()
-
-                # Check against previous 2
-                if color == prevQ[1] and prevQ[1] == prevQ[0]:
-                    continue
-
-                else:
-                    # Else, try this color and move on
-                    copyQ.pop(0)
-                    copyQ.append(color)
-
-                    #print(postsLeft, colors, prevQ)
-                    recurseFn(postsLeft - 1, colors, copyQ)
-
-        # Init recursive fn
-        recurseFn(n, k, [-1, -1])
-
-        return self.total_count
 
 
 s = Solution()
-print(s.numWays(2, 46340))
+print(s.numWays(3, 1235))
---
+++ problems/python/paintFence_brute.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+        self.total_count = 0
+
+        # Set up recursive helper
+        def recurseFn(postsLeft, colors, prevQ):
+
+            # Base case
+            if postsLeft == 0:
+                self.total_count += 1
+                return
+
+            # Recursive Cases
+            for color in range(colors):
+                copyQ = prevQ.copy()
+
+                # Check against previous 2
+                if color == prevQ[1] and prevQ[1] == prevQ[0]:
+                    continue
+
+                else:
+                    # Else, try this color and move on
+                    copyQ.pop(0)
+                    copyQ.append(color)
+
+                    #print(postsLeft, colors, prevQ)
+                    recurseFn(postsLeft - 1, colors, copyQ)
+
+        # Init recursive fn
+        recurseFn(n, k, [-1, -1])
+
+        return self.total_count
+
+
+s = Solution()
+print(s.numWays(3, 2))
